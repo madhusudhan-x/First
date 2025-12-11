0f8f0c88ac91be7a3a6617c3a28e720eecfb8a25
jayyy <jayyy@gmail.com>
2025-12-11 10:18:17 +0530

    added all programs

---
+++ objectproperties.js
@@ -0,0 +1,13 @@
+let employee = {
+    id: 101,
+    name: "Alice",
+    department: "HR"
+};
+let student = {
+    name: "John Doe",
+    age: 20,
+    course: "Computer Science",
+    college: "ABC University"
+};
+console.table(employee);
+console.table(student);
